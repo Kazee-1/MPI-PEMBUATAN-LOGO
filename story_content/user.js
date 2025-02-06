24e78da392a7ec9bbf1bdf8bddbacc8763d863c9
Kazee-1 <kazeegitulohh@gmail.com>
2025-02-06 20:22:17 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,82 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6M9wMA2tXzT":
+        Script1();
+        break;
+      case "6krn1w8Ax3y":
+        Script2();
+        break;
+      case "5UyQI9DQUWQ":
+        Script3();
+        break;
+      case "6mvh8eBGtVx":
+        Script4();
+        break;
+      case "6bYkdi2aaCC":
+        Script5();
+        break;
+      case "5n9kWYEuEHj":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.pause();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.pause();
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+audio.play();
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.pause();
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+audio.play();
+}
+
